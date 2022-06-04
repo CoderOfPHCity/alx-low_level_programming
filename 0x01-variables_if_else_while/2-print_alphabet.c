88ee55aa9550cfbcac5a70780200b37bff96c398
Daniel from Phcity <103477809+CoderOfPHCity@users.noreply.github.com>
2022-06-04 01:18:05 +0100

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description:printing alphabets in lowercase
+ * Return: Return
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+		putchar('\n');
+	return (0);
