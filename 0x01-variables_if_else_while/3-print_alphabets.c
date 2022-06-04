8f8d7a120bf96427e4a3342131daf4cfa220d2b3
Daniel from Phcity <103477809+CoderOfPHCity@users.noreply.github.com>
2022-06-04 02:09:41 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,9 +10,13 @@ int main(void)
 	int ch, CH;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
+	{
 		putchar(ch);
+	}
 	for (CH = 'A'; CH <= 'Z'; CH++)
+	{
 		putchar(CH);
+	}
 		putchar('\n');
 	return (0);
 }
