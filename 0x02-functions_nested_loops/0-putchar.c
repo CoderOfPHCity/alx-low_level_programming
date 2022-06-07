4a08552d884db8e6abf88e3047903641a835ee34
Daniel from Phcity <103477809+CoderOfPHCity@users.noreply.github.com>
2022-06-07 18:56:41 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include"main.h"
-
 /**
  * main - Entry point
  *
@@ -7,15 +6,12 @@
  *
  * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-	char str[] = "_putchar";
-	int ch;
-
-	for (ch = 0; ch < 8; ++ch)
-		_putchar(str[ch]);
-	_putchar('\n');
-
-	return (0);
+char str[] = "_putchar";
+int ch;
+for (ch = 0; ch < 8; ++ch)
+_putchar(str[ch]);
+_putchar('\n');
+return (0);
 }
